a79f258fa8af13ec44484aa21580a6027f848aac
amgarber <agarber@mail.austral.edu.ar>
2025-11-20 19:30:31 -0300

    First changes on SnippetServiceOperations.ts

--- src/utils/queries.tsx
+++ src/utils/queries.tsx
@@ -2,10 +2,10 @@ import {useMutation, UseMutationResult, useQuery} from 'react-query';
 import {CreateSnippet, PaginatedSnippets, Snippet, UpdateSnippet} from './snippet.ts';
 import {SnippetOperations} from "./snippetOperations.ts";
 import {PaginatedUsers} from "./users.ts";
-import {FakeSnippetOperations} from "./mock/fakeSnippetOperations.ts";
 import {TestCase} from "../types/TestCase.ts";
 import {FileType} from "../types/FileType.ts";
 import {Rule} from "../types/Rule.ts";
+import {SnippetServiceOperations} from "./SnippetServiceOperations.ts";
 // import {useAuth0} from "@auth0/auth0-react";
 // import {useEffect} from "react";
 
@@ -21,8 +21,7 @@ export const useSnippetsOperations = () => {
   //         .catch(error => console.error(error));
   // });
 
-  const snippetOperations: SnippetOperations = new FakeSnippetOperations(/* getAccessTokenSilently */); // TODO: Replace with your implementation
-
+  const snippetOperations: SnippetOperations = new SnippetServiceOperations();
   return snippetOperations
 }
 
@@ -74,10 +73,10 @@ export const useShareSnippet = () => {
 };
 
 
-export const useGetTestCases = () => {
+export const useGetTestCases = (snippetId: string) => {
   const snippetOperations = useSnippetsOperations()
 
-  return useQuery<TestCase[] | undefined, Error>(['testCases'], () => snippetOperations.getTestCases(), {});
+  return useQuery<TestCase[] | undefined, Error>(['testCases'], () => snippetOperations.getTestCases(snippetId), {});
 };
 
 
