3620013bffee5bf952b84ad20d24d37e13f2e5b3
amgarber <agarber@mail.austral.edu.ar>
2025-12-14 01:07:55 -0300

    fix process error

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,20 @@
-export const FRONTEND_URL = process?.env?.FRONTEND_URL ?? "https://ingsis25.duckdns.org"
-export const BACKEND_URL = process?.env?.BACKEND_URL ?? "http://localhost:8001/api"
-export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME ?? "test@gmail.com"
-export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD ?? "Ingenieria2025!"
-export const SNIPPETS_SERVICE_URL = process?.env?.VITE_SNIPPETS_SERVICE_URL ?? "http://localhost:8001"
-export const RUNNER_SERVICE_URL = process?.env?.VITE_RUNNER_SERVICE_URL ?? "http://localhost:8000"
-export const VITE_AUTH0_AUDIENCE =  process?.env?.VITE_AUTH0_AUDIENCE ?? "https://snippet-searcher.api"
+export const FRONTEND_URL =
+    import.meta.env.VITE_FRONTEND_URL ?? "https://ingsis25.duckdns.org"
+
+export const BACKEND_URL =
+    import.meta.env.VITE_BACKEND_URL ?? "https://ingsis25.duckdns.org/api"
+
+export const AUTH0_USERNAME =
+    import.meta.env.VITE_AUTH0_USERNAME ?? "test@gmail.com"
+
+export const AUTH0_PASSWORD =
+    import.meta.env.VITE_AUTH0_PASSWORD ?? "Ingenieria2025!"
+
+export const SNIPPETS_SERVICE_URL =
+    import.meta.env.VITE_SNIPPETS_SERVICE_URL ?? "http://localhost:8001"
+
+export const RUNNER_SERVICE_URL =
+    import.meta.env.VITE_RUNNER_SERVICE_URL ?? "http://localhost:8000"
+
+export const VITE_AUTH0_AUDIENCE =
+    import.meta.env.VITE_AUTH0_AUDIENCE ?? "https://snippet-searcher.api"
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.FRONTEND_URL': JSON.stringify(env.FRONTEND_URL),
-      'process.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL)
-    },
+export default defineConfig({
     plugins: [react()],
-      server : {
-          proxy: {
-              '/api': {
-                  target: 'http://localhost:8001',
-                  changeOrigin: true,
-                  rewrite: (path: string) => path.replace(/^\/api/, ''),
-
-                  configure: (proxy, _options) => {
-                      proxy.on('error', (err, _req, _res) => {
-                          console.log('proxy error', err);
-                      });
-                      proxy.on('proxyReq', (proxyReq, req, _res) => {
-                          console.log('Sending Request to the Target:', req.method, req.url);
-                      });
-                      proxy.on('proxyRes', (proxyRes, req, _res) => {
-                          console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-                      });
-                  },
-              },
-          }
-      }
-  }
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8001',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
